31b5a1b5f3dcac787bbc17dbd5ea68b558a00e09
Jonathan Lopez <jonathan.lopez.1224@gmail.com>
2021-03-19 13:48:06 -0400

    Add files via upload

---
+++ Test/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    float x = 3.299;
+    float z;
+    z= x*100;
+    unsigned int y, a;
+    a = (unsigned int) z;
+    y = (unsigned int) x;
+    printf("%d", a);
+    //printf("Hello, World!\n");
+    return 0;
+}
